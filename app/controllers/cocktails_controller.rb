6062a9151b74897abda4d26b9e4ba598c549e988
Filip Ilinca <filip.ilinca@protonmail.com>
2018-08-17 16:51:23 +0100

    customary save, dose controller left to code

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,6 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Coctkail.all
+    @cocktails = Cocktail.all
   end
 
   def show
@@ -12,13 +12,17 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktails = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktails_path
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "cocktails", to: "cocktails#index"
-  get "cocktails/:id", to: "cocktails#show"
-  get "cocktails/new", to: "cocktails#new"
-  post "cocktails", to: "cocktails#create"
-  #resources :cocktails, only: [:index, :show, :new, :create]
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "cocktails", to: "cocktails#index"
+  # get "cocktails/:id", to: "cocktails#show"
+  # get "cocktails/new", to: "cocktails#new"
+  # post "cocktails", to: "cocktails#create"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
